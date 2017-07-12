8372fe66015d7cae96295d2ac78f53f359128018
Vishal Shah <spk83@users.noreply.github.com>
2017-07-12 19:15:08 -0400

    Fixes andrewrothstein/ansible-galaxy-local-deps#1 - indent using spaces (#2)

--- ansiblegalaxylocaldeps/writedeps.py
+++ ansiblegalaxylocaldeps/writedeps.py
@@ -21,24 +21,24 @@ def run():
     with open(meta_main, 'r') as f:
       y = yaml.load(f)
       if 'dependencies' in y:
-	log.info('found {0} dependencies...'.format(len(y['dependencies'])))
-	o = []
-	for d in y['dependencies']:
-	  key = effkey(d)
+        log.info('found {0} dependencies...'.format(len(y['dependencies'])))
+        o = []
+        for d in y['dependencies']:
+          key = effkey(d)
           if key:
-	    r = d[key]
+            r = d[key]
             if 'version' in d:
               o.append({'src' : r, 'version' : d['version']})
             else:
               o.append({'src' : r})
           else:
             log.warn('ignoring {0}'.format(d))
-
-	log.info('writing out requirements.yml...')
-	with open('requirements.yml', 'w') as r:
+        
+        log.info('writing out requirements.yml...')
+        with open('requirements.yml', 'w') as r:
           r.write(yaml.dump(o))
       else:
-	log.info('no dependencies')
+        log.info('no dependencies')
   else:
     log.info('{0} not found'.format(meta_main))
 
