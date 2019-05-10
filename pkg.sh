1831c14bf4393d3fa43199ccbd8beb5a991ddbef
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-05-09 22:25:08 -0500

    more cleanup

--- pkg.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-rm -rf dist
-python setup.py sdist
-twine upload dist/*
--- src/ansiblegalaxylocaldeps/writedeps.py
+++ src/ansiblegalaxylocaldeps/writedeps.py
@@ -2,8 +2,10 @@ import sys
 import yaml
 import os
 import logging
+import argparse
+from typing import Union
 
-def effkey(d):
+def effkey(d) -> Union[str, None]:
   if 'role' in d:
     return 'role'
   elif 'src' in d:
@@ -11,40 +13,56 @@ def effkey(d):
   else:
     return None
 
-def run():
-  log = logging.getLogger("ansible-galaxy-local-deps-write")
+def find(role_dir: str, f: str) -> Union[str, None]:
+  log = logging.getLogger('ansible-galaxy-local-deps-write.find')
+  fq = os.path.join(role_dir, f)
+  log.info('looking for {0}...'.format(fq))
+  if os.path.isfile(fq):
+    return fq
+  return None
 
-  meta_main = 'meta/main.yml'
-  log.info('looking for {0}...'.format(meta_main))
-  if os.path.isfile(meta_main):
+def find_meta_main(role_dir: str):
+  return find(role_dir, os.path.join('meta', 'main.yml'))
+
+def extract_dependencies(y):
+  log = logging.getLogger('ansible-galaxy-local-deps-write.extract_dependencies')
+  o = []
+  if 'dependencies' in y:
+    for d in y['dependencies']:
+      key = effkey(d)
+      if key:
+        r = d[key]
+        if 'version' in d:
+          o.append({'role' : r, 'version' : d['version']})
+        else:
+          o.append({'role' : r})
+      else:
+        log.warn('ignoring key: {0}'.format(d))
+  return o
+
+def run(role_dir: str):
+  log = logging.getLogger('ansible-galaxy-local-deps-write.run')
+  meta_main = find_meta_main(role_dir)
+  if meta_main is not None:
     log.info('found. looking for dependencies in {0}...'.format(meta_main))
     with open(meta_main, 'r') as f:
-      y = yaml.load(f)
-      if 'dependencies' in y:
-        log.info('found {0} dependencies...'.format(len(y['dependencies'])))
-        o = []
-        for d in y['dependencies']:
-          key = effkey(d)
-          if key:
-            r = d[key]
-            if 'version' in d:
-              o.append({'src' : r, 'version' : d['version']})
-            else:
-              o.append({'src' : r})
-          else:
-            log.warn('ignoring {0}'.format(d))
-        
-        log.info('writing out requirements.yml...')
-        with open('requirements.yml', 'w') as r:
-          r.write(yaml.dump(o))
-      else:
-        log.info('no dependencies')
-  else:
-    log.info('{0} not found'.format(meta_main))
+      y = yaml.load(f, Loader=yaml.FullLoader)
+    o = extract_dependencies(y)
+    of = os.path.join(role_dir, 'requirements.yml')
+    log.info('writing out {0} dependencies to {1}...'.format(len(o), of))
+    with open(of, 'w') as w:
+      w.write(yaml.dump(o))
 
 def main():
   logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
   )
-  run()
+
+  parser = argparse.ArgumentParser(
+    description='generates a requirements.yml from an Ansible roles meta/main.yml file'
+  )
+  parser.add_argument('roledirs', nargs='+', default=['.'])
+  args = parser.parse_args()
+  for roledir in args.roledirs:
+    run(roledir)
